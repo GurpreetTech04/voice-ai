be42d12a7b6af68780b2444011f340b49a6b3efa
GurpreetTech04 <labbi2011@gmail.com>
2026-01-31 16:28:18 +0530

    Implement text-to-speech for audio transcriptions
    Added text-to-speech functionality to read transcriptions aloud.

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import streamlit as st
 import os
 from audio_text import transcribe_audio_with_groq
 # from rag_system import rag_system
+import pyttsx3
+engine = pyttsx3.init() # object creation
 
 audio =  st.audio_input("Speek",  sample_rate=16000, width="stretch")
+
+def speak(text):
+    
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
+    
 
 if audio:
     audio_bytes = audio.read()
@@ -12,5 +21,9 @@ if audio:
     if text:
         st.markdown("**Transcription:**")
         st.write(text)
+        speak(text)
     else:
+        
         st.error(f"Transcription Error: {error}")
+
+
